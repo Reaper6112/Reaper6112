3acd26a8ec18b556a36456a359973d7e17196717
Reaper6112 <ppamidipati@uwaterloo.ca>
2022-03-11 19:58:20 -0500

    Practice for SQL basic functions.

--- SQL_SchoolDatabase/SchoolDatabase.sql
+++ SQL_SchoolDatabase/SchoolDatabase.sql
@@ -1,5 +1,6 @@
 create database School;
-drop table AGENTS;
+
+create table AGENTS;
 
 CREATE TABLE  AGENTS 
    (	
@@ -13,6 +14,14 @@ CREATE TABLE  AGENTS
 
 select * from AGENTS;
 
+select AGENT_NAME 
+from AGENTS
+where WORKING_AREA = 'Bangalore' OR 'London';
+
+select AGENT_NAME 
+from AGENTS
+where not WORKING_AREA = 'Bangalore';
+
 INSERT INTO AGENTS VALUES ('A007', 'Ramasundar', 'Bangalore', '0.15', '077-25814763', '');
 INSERT INTO AGENTS VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969', '');
 INSERT INTO AGENTS VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365', '');
@@ -25,3 +34,43 @@ INSERT INTO AGENTS VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964', '
 INSERT INTO AGENTS VALUES ('A006', 'McDen', 'London', '0.15', '078-22255588', '');
 INSERT INTO AGENTS VALUES ('A004', 'Ivan', 'Torento', '0.15', '008-22544166', '');
 INSERT INTO AGENTS VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178', '');
+
+select count(AGENT_CODE) from AGENTS;
+
+select avg(COMMISSION) from AGENTS;
+
+select sum(COMMISSION) from AGENTS;
+
+select AGENT_name, min(COMMISSION) from AGENTS;
+
+select AGENT_NAME 
+from AGENTS
+Group by WORKING_AREA
+having avg(COMMISSION)> 0.12;
+
+select AGENT_NAME 
+from AGENTS
+Group by WORKING_AREA
+order by WORKING_AREA desc;
+
+select AGENT_NAME 
+from AGENTS
+where WORKING_AREA is not NULL;
+
+update AGENTS
+set AGENT_NAME = 'Amar', WORKING_AREA = 'Delhi'
+where AGENT_CODE = 'A001';
+
+delete from AGENTS
+where AGENT_CODE = 'A002';
+
+select AGENT_NAME 
+from AGENTS
+where WORKING_AREA in ('Delhi', 'Bangalore', 'Mumbai', 'Chennai');
+
+select AGENT_CODE, AGENT_NAME 
+from AGENTS
+where COMMISSION between 0.12 and 0.14;
+
+select AGENT_NAME as A_name
+from AGENTS
