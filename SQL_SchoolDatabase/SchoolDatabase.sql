a95f58067503a1f33c0cde1f452212d4a14503b3
Reaper6112 <ppamidipati@uwaterloo.ca>
2022-03-25 18:00:29 -0700

    Join command practice

--- SQL_SchoolDatabase/SchoolDatabase.sql
+++ SQL_SchoolDatabase/SchoolDatabase.sql
@@ -1,76 +1,66 @@
 create database School;
+drop table Courses;
 
-create table AGENTS;
-
-CREATE TABLE  AGENTS 
+CREATE TABLE  Courses
    (	
-    AGENT_CODE VARCHAR(6) NOT NULL PRIMARY KEY, 
-	AGENT_NAME VARCHAR(40), 
-	WORKING_AREA VARCHAR(35), 
-	COMMISSION FLOAT(10,2), 
-	PHONE_NO VARCHAR(15), 
-	COUNTRY VARCHAR(25) 
+    ID VARCHAR(6) NOT NULL PRIMARY KEY ,
+    Student_ID VARCHAR(6) NOT NULL,
+	Course_name VARCHAR(40), 
+	City VARCHAR(35), 
+	credit FLOAT(10,2)
+    #FOREIGN KEY (Student_ID) REFERENCES Students(ID)
 	 );
 
-select * from AGENTS;
+select * from STUDENTS;
+select * from COURSES;
 
-select AGENT_NAME 
-from AGENTS
-where WORKING_AREA = 'Bangalore' OR 'London';
+INSERT INTO Students VALUES ('A007', 'Ramasundar', 'Bangalore', '0.15', '077-25814763');
+INSERT INTO Students VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969');
+INSERT INTO Students VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365');
+INSERT INTO Students VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874');
+INSERT INTO Students VALUES ('A010', 'Santakumar', 'Chennai', '0.14', '007-22388644');
+INSERT INTO Students VALUES ('A012', 'Lucida', 'San Jose', '0.12', '044-52981425');
+INSERT INTO Students VALUES ('A005', 'Anderson', 'Brisban', '0.13', '045-21447739');
+INSERT INTO Students VALUES ('A001', 'Subbarao', 'Bangalore', '0.14', '077-12346674');
+INSERT INTO Students VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964');
+INSERT INTO Students VALUES ('A006', 'McDen', 'London', '0.15', '078-22255588');
+INSERT INTO Students VALUES ('A004', 'Ivan', 'Torento', '0.15', '008-22544166');
+INSERT INTO Students VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178');
 
-select AGENT_NAME 
-from AGENTS
-where not WORKING_AREA = 'Bangalore';
+INSERT INTO Courses VALUES ('1','A0001', 'Physics', 'Bangalore', '0.01');
+INSERT INTO Courses VALUES ('2','A001', 'Light', 'Bangalore', '0.02');
+INSERT INTO Courses VALUES ('3','A001', 'Electricity', 'Bangalore', '0.01');
+INSERT INTO Courses VALUES ('4','A001', 'Calculus', 'Bangalore', '0.01');
+INSERT INTO Courses VALUES ('5','A001', 'English', 'Bangalore', '0.01');
 
-INSERT INTO AGENTS VALUES ('A007', 'Ramasundar', 'Bangalore', '0.15', '077-25814763', '');
-INSERT INTO AGENTS VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969', '');
-INSERT INTO AGENTS VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365', '');
-INSERT INTO AGENTS VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874', '');
-INSERT INTO AGENTS VALUES ('A010', 'Santakumar', 'Chennai', '0.14', '007-22388644', '');
-INSERT INTO AGENTS VALUES ('A012', 'Lucida', 'San Jose', '0.12', '044-52981425', '');
-INSERT INTO AGENTS VALUES ('A005', 'Anderson', 'Brisban', '0.13', '045-21447739', '');
-INSERT INTO AGENTS VALUES ('A001', 'Subbarao', 'Bangalore', '0.14', '077-12346674', '');
-INSERT INTO AGENTS VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964', '');
-INSERT INTO AGENTS VALUES ('A006', 'McDen', 'London', '0.15', '078-22255588', '');
-INSERT INTO AGENTS VALUES ('A004', 'Ivan', 'Torento', '0.15', '008-22544166', '');
-INSERT INTO AGENTS VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178', '');
+INSERT INTO Courses VALUES ('6','A012', 'Mathematics', 'Mumbai', '0.01');
+INSERT INTO Courses VALUES ('7','A012', 'English', 'Mumbai', '0.01');
+INSERT INTO Courses VALUES ('8','A012', 'Science', 'Mumbai', '0.02');
+INSERT INTO Courses VALUES ('9','A012', 'Art', 'Mumbai', '0.01');
+INSERT INTO Courses VALUES ('10','A012', 'Aerodynamics', 'Mumbai', '0.01');
 
-select count(AGENT_CODE) from AGENTS;
+INSERT INTO Courses VALUES ('11','A008', 'Biology', 'London', '0.02');
+INSERT INTO Courses VALUES ('12','A008', 'Chemistry', 'London', '0.01');
+INSERT INTO Courses VALUES ('13','A008', 'English', 'London', '0.03');
+INSERT INTO Courses VALUES ('14','A008', 'Data Science', 'London', '0.01');
 
-select avg(COMMISSION) from AGENTS;
+INSERT INTO Courses VALUES ('15','A007', 'Film Intro 101', 'Toronto', '0.01');
+INSERT INTO Courses VALUES ('16','A007', 'Acting and Directing', 'Toronto', '0.01');
+INSERT INTO Courses VALUES ('17','A007', 'Engineering 101', 'Toronto', '0.02');
+INSERT INTO Courses VALUES ('18','A007', 'Advanced Piano', 'Toronto', '0.01');
+INSERT INTO Courses VALUES ('19','A007', 'English and Writing', 'Toronto', '0.01');
 
-select sum(COMMISSION) from AGENTS;
+SELECT S.ID, S.ENAME, C.CREDIT, C.ID, C.COURSE_NAME
+FROM Students as S 
+RIGHT OUTER JOIN COURSES AS C 
+ON S.ID = C.Student_ID;
 
-select AGENT_name, min(COMMISSION) from AGENTS;
+SELECT S.ID, S.ENAME, C.CREDIT, C.ID, C.COURSE_NAME
+FROM Students as S 
+LEFT OUTER JOIN COURSES AS C 
+ON S.ID = C.Student_ID;
 
-select AGENT_NAME 
-from AGENTS
-Group by WORKING_AREA
-having avg(COMMISSION)> 0.12;
-
-select AGENT_NAME 
-from AGENTS
-Group by WORKING_AREA
-order by WORKING_AREA desc;
-
-select AGENT_NAME 
-from AGENTS
-where WORKING_AREA is not NULL;
-
-update AGENTS
-set AGENT_NAME = 'Amar', WORKING_AREA = 'Delhi'
-where AGENT_CODE = 'A001';
-
-delete from AGENTS
-where AGENT_CODE = 'A002';
-
-select AGENT_NAME 
-from AGENTS
-where WORKING_AREA in ('Delhi', 'Bangalore', 'Mumbai', 'Chennai');
-
-select AGENT_CODE, AGENT_NAME 
-from AGENTS
-where COMMISSION between 0.12 and 0.14;
-
-select AGENT_NAME as A_name
-from AGENTS;
+SELECT S.ID, S.ENAME, C.CREDIT, C.ID, C.COURSE_NAME
+FROM Students as S 
+INNER JOIN COURSES AS C 
+ON S.ID = C.Student_ID;
